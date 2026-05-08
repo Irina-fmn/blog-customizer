df8c4c0d0f53634f5f0964a5cd62012a7244e4be
Irina Fominykh <irina.fmn96@yandex.ru>
2026-05-08 00:59:38 -0600

    dev

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,36 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import { ArticleStateType } from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
+type Props = {
+	articleState: ArticleStateType;
+	setArticleState: (state: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({ articleState, setArticleState }: Props) => {
+	const [isOpen, setIsOpen] = useState(true);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+		evt.preventDefault();
+
+		setArticleState(articleState);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={toggleMenu} />
+
+			<aside
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
+				<form className={styles.form} onSubmit={handleSubmit}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
